f6b2350780e0583b70b33e1b2804423f9cfcafcd
karnvighnesh <92665956+karnvighnesh@users.noreply.github.com>
2023-10-09 14:22:20 +0000

    variables done

---
+++ basics/variables.js
@@ -0,0 +1,5 @@
+const account_id=14435;
+let accuntEmail="vighnesh@google.com";
+var accountPassword="12345";
+accountcity="bengluru";
+console.table([account_id,accuntEmail,accountPassword,accountcity]);
